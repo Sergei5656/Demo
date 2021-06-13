2a1c9f14f35ceb1443d8556994d3d581289318e7
Sergei5656 <Uchaev_2000@mail.ru>
2021-06-13 12:39:15 +0400

    112

--- Demos/Demos/Windows/MainMenu.xaml.cs
+++ Demos/Demos/Windows/MainMenu.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Demos
+namespace Demos.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainMenu : Window
     {
-        public MainWindow()
+        public MainMenu()
         {
             InitializeComponent();
         }
